c0a91e729a4ec95784fa70750eef24f7cece6150
Valtece <filhovaltece@gmail.com>
2025-05-22 20:16:18 -0300

    update

--- Atividade4.py
+++ Atividade4.py
@@ -6,4 +6,4 @@ consumo_medio = distancia_percorrida / combustivel_gasto
 
 print(f'A distância percorrida foi: {distancia_percorrida:.2f}km')
 print(f'O consumo de Combustível foi: {combustivel_gasto:.2f} litros')
-print(f'E o consumo médio foi de: {consumo_medio:.2f} l/km')
+print(f'E o consumo médio foi de: {consumo_medio:.2f} km/l')
