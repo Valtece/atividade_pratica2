d221655f9a0102d97decc5e531cfe9e38e6ba913
Valtece <filhovaltece@gmail.com>
2025-05-21 21:51:12 -0300

    Atualizar

--- Atividade2.py
+++ Atividade2.py
@@ -8,6 +8,7 @@ valor_desconto = preco_original * (20/100)
 #valor final com desconto
 valor_final = preco_original - valor_desconto
 
+print(f'Produto: {nome_produto}')
 print(f'Preço Original: R${preco_original:.2f}')
 print(f'Desconto de: {porcentagem_desconto}%')
 print(f'Valor do desconto: R${valor_desconto:.2f}')
